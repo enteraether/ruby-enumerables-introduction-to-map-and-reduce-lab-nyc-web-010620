dea3831d03b17bbc1c330736bfd583dc4bc0d5b8
Theresa <thetheresagarcia@gmail.com>
2020-01-03 21:01:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map_to_negativize(source_array)
     i += 1
   end
 
-  puts new
+  return new
 end
 
 
@@ -20,7 +20,7 @@ def map_to_no_change(source_array)
    new.push(source_array[i])
    i += 1
  end
-  puts new
+  new
 end
 
 
@@ -31,7 +31,7 @@ def map_to_double(source_array)
     new.push(source_array[i]*2)
     i += 1
     end
-  puts new
+  new
 end
 
 
