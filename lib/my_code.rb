4111748467ed6855e74a899fb94729783025457b
Theresa <thetheresagarcia@gmail.com>
2020-01-03 21:07:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,6 +20,7 @@ def map_to_no_change(source_array)
    new.push(source_array[i])
    i += 1
  end
+ puts new
   new
 end
 
@@ -31,6 +32,7 @@ def map_to_double(source_array)
     new.push(source_array[i]*2)
     i += 1
     end
+    puts new
   new
 end
 
@@ -42,6 +44,7 @@ def map_to_square(source_array)
     new.push(source_array[i]*source_array[i])
     i += 1
   end
+  puts new
     new
   end
 
